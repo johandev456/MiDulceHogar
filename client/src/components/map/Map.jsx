3bd494bee4eb8eeaca5cfa55e325f44dbbbfb0b7
johandev456 <johanjohanrosariolopez@gmail.com>
2026-08-13 20:02:14 -0400

    fix: pin

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -2,6 +2,17 @@ import { MapContainer, TileLayer } from 'react-leaflet'
 import './map.scss'
 import "leaflet/dist/leaflet.css";
 import Pin from '../pin/Pin';
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 function Map({items}){
   return (
